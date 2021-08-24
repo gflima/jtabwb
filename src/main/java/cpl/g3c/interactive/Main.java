58b5cac329231a6edc728b251087507c93264c4a
Guilherme Lima <Guilherme.Lima@ibm.com>
2021-08-24 15:53:00 -0300

    src: Start coding interactive version of g3c

---
+++ src/main/java/cpl/g3c/interactive/Main.java
@@ -0,0 +1,16 @@
+package cpl.g3c.interactive;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+import org.apache.commons.cli.*;
+
+public class Main {
+
+    // The name of this program.
+    private static String ME = "jtabwb-g3c-top";
+
+    public static void main(String[] args) {
+        System.out.print("hello\n");
+    }
+}
